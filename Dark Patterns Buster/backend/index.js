6e0dfac190bd6922eba72368bd62088ad60f7452
Prajwal Kumar <prasunjaysjc@gmail.com>
2024-01-30 03:41:41 +0530

    Updated backend

--- Dark Patterns Buster/backend/index.js
+++ Dark Patterns Buster/backend/index.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const { scraperUtil } = require('./scraper2.0/index.js');
+const cors = require('cors');
+
 const app = express();
 
 const PORT = 3000;
 
-app.post('/', () => {
-    
+app.use(express.json());
+app.use(cors());
+
+app.post('/url', async (req, res) => {
+    const url = req.body.url;
+    await scraperUtil(url);
+    res.send();
 });
 
 app.listen(PORT, () => {
--- Dark Patterns Buster/index.js
+++ Dark Patterns Buster/index.js
@@ -2,7 +2,17 @@ document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("go-button").addEventListener("click", getUrl);
 });
 
-function getUrl() {
-    var url = document.getElementById("url").value;
-    console.log(url);
+async function getUrl() {
+    const urlElement = document.getElementById("url");
+    var url = urlElement.value;
+    urlElement.value = "";
+    const response = await fetch("http://localhost:3000/url", {
+        method : "POST",
+        body: JSON.stringify({
+            url : url
+        }),
+        headers:{
+            "Content-type" : "application/json; charset=UTF-8",
+        }
+    });
 }
